2bdb6b3813ff626a4febb09f613b89561c51f0dd
Hwagu <kjs10033@naver.com>
2022-06-18 21:22:54 +0900

    디자인 구현

---
+++ app/src/main/java/com/example/evpoly_app/Task.java
@@ -0,0 +1,50 @@
+package com.example.evpoly_app;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Task extends AsyncTask<String, Void, String> {
+
+    String clientKey = "#########################";;
+    private String str, receiveMsg;
+    private final String ID = "########";
+
+    @Override
+    protected String doInBackground(String... params) {
+        URL url = null;
+        try {
+            //url = new URL("https://jsonplaceholder.typicode.com/users"); // 서버 URL
+            url = new URL("http://10.1.3.64:8080/nowparking"); // 서버 URL
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            conn.setRequestProperty("x-waple-authorization", clientKey);
+
+            if (conn.getResponseCode() == conn.HTTP_OK) {
+                InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                BufferedReader reader = new BufferedReader(tmp);
+                StringBuffer buffer = new StringBuffer();
+                while ((str = reader.readLine()) != null) {
+                    buffer.append(str);
+                }
+                receiveMsg = buffer.toString();
+                Log.i("receiveMsg : ", receiveMsg);
+
+                reader.close();
+            } else {
+                Log.i("결과", conn.getResponseCode() + "Error");
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return receiveMsg;
+    }
+}
